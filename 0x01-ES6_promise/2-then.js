683ce75034bb058166dc457d294e9f8685fad564
ayegbakenneth <ayegbakenneth@gmail.com>
2024-06-26 16:09:28 +0000

    This code illustrate on how to use the .then and .catch method

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return { status: 200, body: response };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error('');
+    });
+}
