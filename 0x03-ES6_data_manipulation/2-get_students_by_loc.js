535bf5fb0ccbf7437b85d01b07a94c9b877ac913
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-02 12:11:24 +0000

    New update on how to use the filter method of an array

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-import getListStudents from './0-get_list_students.js';
-
 export default function getStudentsByLocation(list, city) {
   return list.filter((student) => student.location === city);
 }
