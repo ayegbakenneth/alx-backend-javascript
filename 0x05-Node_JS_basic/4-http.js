602f62732606f5ce5f00d75fb7e5fb6bec426e8a
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-20 09:03:06 +0000

    How to create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// File executable path
+
+const http = require('http')
+const app = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello Holberton School!');
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+	console.log(Server running at PORT 1245);
+});
