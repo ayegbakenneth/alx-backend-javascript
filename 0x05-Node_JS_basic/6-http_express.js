455892bd7f72c0c3a8d9f5688f6f7fefad38b964
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-20 13:43:26 +0000

    Fixing up found new issues

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const PORT = 1245
-const app = express()
+const PORT = 1245;
+const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello Holberton School!')
+	res.send('Hello Holberton School!');
 });
 app.listen(PORT, () => {
 	console.log(
-		`The express server is listening on PORT ${PORT}`)
+		`The express server is listening on PORT ${PORT}`);
 });
 
 module.exports = app;
