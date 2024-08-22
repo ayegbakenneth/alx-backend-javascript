89e0a593c83baa9d2ac3d528e985c35fa8724eee
ayegbakenneth <ayegbakenneth@gmail.com>
2024-08-22 08:56:58 +0000

    New feature of the function tested

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,14 +1,34 @@
-const calculateNumber = (type, a, b) => {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  }
-  if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  }
-  if (type === 'DIVIDE') {
-    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
-  }
-  return 0;
-};
+const SUM = 'SUM';
+const SUBTRACT = 'SUBTRACT';
+const DIVIDE = 'DIVIDE';
 
-module.exports = calculateNumber;
+function isNegZero(n) {
+  n = Number(n);
+  return n === 0 && 1 / n === -Infinity;
+}
+
+module.exports = function calculateNumber(type, a, b = 0) {
+  let firstNumber = Number(a);
+  let secondNumber = Number(b);
+
+  if (Number.isNaN(firstNumber) || Number.isNaN(secondNumber))
+    throw TypeError('Parameters must be numbers or able to coerce to number');
+
+  firstNumber = Math.round(firstNumber);
+  secondNumber = Math.round(secondNumber);
+
+  switch (type) {
+    case SUM:
+      return firstNumber + secondNumber;
+    case SUBTRACT:
+      return firstNumber - secondNumber;
+    case DIVIDE:
+      if (secondNumber === 0) return 'ERROR';
+      const quotient = firstNumber / secondNumber;
+      return isNegZero(quotient) ? 0 : quotient;
+    default:
+      throw Error(
+        'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+      );
+  }
+};
